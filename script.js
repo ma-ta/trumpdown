6dee09095fee14528c1fdf9c98fd177bff07599b
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-01-27 14:01:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,25 +8,25 @@ var x = setInterval(function() {
     var now = new Date().getTime();
 
     
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
+    // Find the delta between now an the count down date
+    var delta = countDownDate - now;
     
     // Time calculations for years, days, hours, minutes and seconds
     
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var days = Math.floor(delta / (1000 * 60 * 60 * 24));
     var years = Math.floor(days / 365);
     days = days % 365;
 
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var hours = Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((delta % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="counter-trump"
     document.getElementById("counter-trump").innerHTML = years + " year(s) " + days + "d<br />" + hours + "h "
     + minutes + "m " + seconds + "s ";
     
     // If the count down is over, write some text
-    if (distance <= 0) {
+    if (delta <= 0) {
         clearInterval(x);
         document.getElementById("counter-trump").innerHTML = "Crazy Donald<br>is fully retired!";
         document.body.style.filter = "grayscale(100%)";
