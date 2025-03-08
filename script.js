084d46bdc2f5dd599ac5fd33123ff806acc09af7
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-03-08 18:32:29 +0100

    -

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 // Set the date we're counting down to
-var countDownDate = new Date("2029-01-20T12:00:00-05:00").getTime();
+let countDownDate = new Date("2029-01-20T12:00:00-05:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+let x = setInterval(function() {
 
     // Get todays date and time
-    var now = new Date().getTime();
+    let now = new Date().getTime();
 
-    
+
     // Find the delta between now an the count down date
-    var delta = countDownDate - now;
-    
+    let delta = countDownDate - now;
+
     // Time calculations for years, days, hours, minutes and seconds
-    
-    var days = Math.floor(delta / (1000 * 60 * 60 * 24));
-    var years = Math.floor(days / 365);
+
+    let days = Math.floor(delta / (1000 * 60 * 60 * 24));
+    let years = Math.floor(days / 365);
     days = days % 365;
 
-    var hours = Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((delta % (1000 * 60)) / 1000);
-    
+    let hours = Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((delta % (1000 * 60)) / 1000);
+
+    // Format time
+    let formattedTime = String(hours).padStart(2, "0")   + ":"
+                      + String(minutes).padStart(2, "0") + ":"
+                      + String(seconds).padStart(2, "0");
+
     // Output the result in an element with id="counter-trump"
-    document.getElementById("counter-trump").innerHTML = years + "&nbsp;year(s) " + days + "&nbsp;d<br />" + hours + ":"
-    + minutes + ":" + seconds;
-    
+    document.getElementById("counter-trump").innerHTML = years + "&nbsp;year(s) " + days + "&nbsp;d<br />"
+    + formattedTime;
+
     // If the count down is over, write some text
     if (delta <= 0) {
         clearInterval(x);
